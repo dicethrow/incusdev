d5753c6c5e2824fb61fb401afa5e63912ee6f335
dicethrow <13843062+dicethrow@users.noreply.github.com>
2022-02-01 11:01:05 +1100

    The function this calls now defaults to using 'rsync --delete', which is often dangerous. Now the command line interface requires manually specifying whether to delete or not.

--- lxdev/standalone_cli.py
+++ lxdev/standalone_cli.py
@@ -3,8 +3,10 @@ import lxdev
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("task", type=str, help="action to do, out of: check_dirs, rsync_to_container")
-	parser.add_argument("arg2", type=str, help="multi-purpose second argument, out of: lxd_doc-dev (or any other hostname of lxd container)")
+	parser.add_argument("task", type=str, help="action to do, out of: check_dirs, 'rsync_to_container' and 'rsync_from_container'")
+	parser.add_argument("remote_hostname", type=str, help="remote_hostname")
+	parser.add_argument("if_delete", type=str, help="'delete' or 'keep'? how to handle overwriting files at destination.")
+
 	args = parser.parse_args()
 
 	if args.task == "check_dirs":
@@ -13,20 +15,31 @@ def main():
 		print(f"User dir is: {os.path.expanduser('~')}")
 		print(f"Script called from {os.getcwd()}")
 
-	elif args.task == "rsync_to_container":
+	elif args.task in ["rsync_to_container", "rsync_from_container"]:
 		assert "home" in os.getcwd(), "this function is defined for folders within a host users home directory only"
 		
-		lxd_container_name = assert_we_can_extract_lxd_name_from_hostname(args.arg2)
+		lxd_container_name = assert_we_can_extract_lxd_name_from_hostname(args.remote_hostname)
+
+		if args.if_delete == "delete":
+			delete = True
+		elif args.if_delete == "keep":
+			delete = False
+		else:
+			assert 0, "Invalid if_delete argument passed, should be 'delete' or 'keep'"
 
 		with  lxdev.RemoteClient(
-			host = args.arg2, # e.g. lxd_doc-dev
+			host = args.remote_hostname, # e.g. lxd_doc-dev
 			lxd_container_name = lxd_container_name,
 			local_working_directory = os.getcwd() # the directory where this is called from
 			) as ssh_remote_client:
 
 				# print("Connected!")
-				ssh_remote_client.rsync_to_container()
-			
+				if args.task == "rsync_to_container":
+					ssh_remote_client.rsync_to_container(delete=delete)
+				elif args.task == "rsync_from_container":
+					ssh_remote_client.rsync_from_container(delete=delete)
+				else:
+					assert 0	
 	else:
 		assert 0, "Invalid task given"
 
